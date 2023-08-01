8ddb5fe78f35471fbf975a404970a94ba2b91743
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-08-01 15:06:38 +0000

    add stateless component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting(){
+//     return <h1>This is React!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>This is react!</h1>;
+
+export default FunctionalGreeting;
