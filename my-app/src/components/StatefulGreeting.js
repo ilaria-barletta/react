4f6be782414554f0ae7d3c1a319d4b4319773620
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-08-02 13:03:34 +0000

    update state

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,11 +10,21 @@ class StatefulGreeting extends React.Component {
         };
 
     }
+
+    handleClick(){
+    this.setState({
+            introduction: "goodbye!"
+        });
+        console.log(this.state.introduction);
+
+    }
+
+
     render () {
         return (
             <div>
                 <h1> {this.state.introduction}, {this.props.greeting} {this.props.name}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
