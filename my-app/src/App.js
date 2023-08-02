b097e2f87a412fedd9e652e4744f0154d53ca3ff
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-08-02 12:28:07 +0000

    use class component to dublicate functional component funcionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreeting greeting="this is how i work, call it magic!" name="ily" />
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="good to meet you" name="Ily" />
     </div>
