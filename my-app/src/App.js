0dd6d40e2e5a507ea5f21fa8c40951e5e2f1428d
ilaria-barletta <ilariateresabarletta@gmail.com>
2023-08-04 09:26:02 +0000

    event handling in functional and class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <StatefulGreeting greeting="this is how i work, call it magic!" name="ily" />
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="good to meet you" name="Ily" />
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
